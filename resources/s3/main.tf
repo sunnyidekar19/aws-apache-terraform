b48df173973d79597b70fe665f8f5b6d404b3cc8
sunnyidekar19 <sunnyidekar19@gmail.com>
2021-09-26 13:20:38 +0100

    update s3 module to create additional bucket depending on user role

--- resources/s3/main.tf
+++ resources/s3/main.tf
@@ -1,3 +1,5 @@
+resource "random_pet" "main" {}
+
 resource "aws_s3_bucket" "main" {
   acl = "private"
   server_side_encryption_configuration {
@@ -23,4 +25,8 @@ resource "aws_s3_bucket_object" "main" {
   key                    = "${var.role}.pub"
   source                 = var.public_key_path
   server_side_encryption = "aws:kms"
+}
+
+resource "aws_s3_bucket" "user" {
+  bucket = "${random_pet.main.id}-test"
 }
